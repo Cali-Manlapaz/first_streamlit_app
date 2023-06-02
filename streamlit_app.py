acd72c9710b330c068fb8cd72ac031ecc7a637fe
Cali-Manlapaz <134988698+Cali-Manlapaz@users.noreply.github.com>
2023-06-02 14:02:49 +0800

    Update streamlit_app.py
    fixed typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,4 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT  * From fruit_load_list")
 my_data_rows = my_cur.fetchoall() #fetch all data from fruit load list table
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
